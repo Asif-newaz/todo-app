192895e6471abb984e5a4ff48210ce5ffe4c2bd4
Asif-newaz <asif.newaz0093@gmail.com>
2022-10-17 23:58:43 +0600

    started writing js

---
+++ script.js
@@ -0,0 +1,12 @@
+// "use strict";
+
+const parent = document.querySelector("#items");
+// const items = document.querySelectorAll(".items");
+// items.style.display = "none";
+
+// parent.style.backgroundColor = "red";
+
+const children = parent.children;
+console.log(children);
+
+// children[0].style.backgroundColor = "violet";
