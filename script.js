c28636a698673cc202118b0f759fd5acc3ca36c0
Asif-newaz <asif.newaz0093@gmail.com>
2022-10-19 02:27:47 +0600

    work with append and appendChild

--- script.js
+++ script.js
@@ -11,9 +11,7 @@ divElement.className = "container";
 divElement.setAttribute("id", "box");
 
 const container = document.querySelector(".todo-list");
-const h2Element = container.querySelector("h2");
-
-container.insertBefore(divElement, h2Element);
-
-divElement.textContent = "Hey there, I'm here to join with you guys!"
- 
+// const h2Element = container.querySelector("h2");
+// container.insertBefore(divElement, h2Element);
+// container.appendChild(divElement);
+container.append(divElement, document.createElement("p"), "Hello world!");
