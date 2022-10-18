32b17b75ff64c8efb3788e5992807d66a8313c9f
Asif-newaz <asif.newaz0093@gmail.com>
2022-10-19 02:16:21 +0600

    work with createElement

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const childrenOne = document.querySelector(".item");
+// const childrenOne = document.querySelector(".item");
 
-const childrenTwo = childrenOne.previousElementSibling;
+// const childrenTwo = childrenOne.previousElementSibling;
 
-childrenTwo.style.color = "crimson";
+// childrenTwo.style.color = "crimson";
 
-console.log(childrenTwo);
+// console.log(childrenTwo);
+
+const divElement = document.createElement("div");
+divElement.className = "container";
+divElement.setAttribute("id", "box");
+
+const container = document.querySelector(".todo-list");
+const h2Element = container.querySelector("h2");
+
+container.insertBefore(divElement, h2Element);
+
+divElement.textContent = "Hey there, I'm here to join with you guys!"
+ 
