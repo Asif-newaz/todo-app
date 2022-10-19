f54f9aff61b365d6879eaa5761cee4bd708c3fb2
Asif-newaz <asif.newaz0093@gmail.com>
2022-10-20 00:02:33 +0600

    work with js event

--- script.js
+++ script.js
@@ -6,12 +6,17 @@
 
 // console.log(childrenTwo);
 
-const divElement = document.createElement("div");
-divElement.className = "container";
-divElement.setAttribute("id", "box");
+// const divElement = document.createElement("div");
+// divElement.className = "container";
+// divElement.setAttribute("id", "box");
 
-const container = document.querySelector(".todo-list");
+// const container = document.querySelector(".todo-list");
 // const h2Element = container.querySelector("h2");
 // container.insertBefore(divElement, h2Element);
 // container.appendChild(divElement);
-container.append(divElement, document.createElement("p"), "Hello world!");
+// container.append(divElement, document.createElement("p"), "Hello world!");
+const headerElement = document.querySelector("#header");
+
+headerElement.addEventListener("mouseover", (event) => {
+  console.log(event);
+});
