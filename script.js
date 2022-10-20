eb3c2f11359c8beb3b37f46ff603b34255a96324
Asif-newaz <asif.newaz0093@gmail.com>
2022-10-21 02:27:32 +0600

    making todo list functional

--- script.js
+++ script.js
@@ -15,8 +15,38 @@
 // container.insertBefore(divElement, h2Element);
 // container.appendChild(divElement);
 // container.append(divElement, document.createElement("p"), "Hello world!");
-const headerElement = document.querySelector("#header");
+// const headerElement = document.querySelector("#header");
 
-headerElement.addEventListener("mouseover", (event) => {
-  console.log(event);
-});
+// headerElement.addEventListener("mouseover", (event) => {
+//   console.log(event);
+// });
+// ---The end---
+
+// Select elements and them to variables
+const newTask = document.querySelector("#new-task");
+const form = document.querySelector("form");
+const todoUl = document.querySelector("#items");
+const completeUL = document.querySelector(".complete-list ul");
+
+const createTask = (task) => {
+  const listItem = createElement("li");
+  const checkBox = createElement("checkbox");
+  const label = createElement("label");
+
+  checkBox.type = "input";
+  label.innerText = task;
+
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
+
+  return listItem;
+};
+
+const addTask = (event) => {
+  event.preventDefault();
+  let listItem = createTask(newTask.value);
+  todoUl.appendChild(listItem);
+  newTask.value = "";
+  // bind the new list item to the incomplete list
+  
+};
